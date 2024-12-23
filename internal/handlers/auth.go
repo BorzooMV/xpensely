2f41fbd3c5eaa46336448d322eb424b7a1ef00c6
Borzoo Moazami <borzoomoazami94@gmail.com>
2024-12-23 21:57:45 +0330

    chore: add comment to encrypt user password

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -48,6 +48,7 @@ func (h *AuthHandler) AuthenticateUser(e echo.Context) error {
 		return utils.SendErrorResponse(e, http.StatusInternalServerError, fmt.Sprintf("Couldn't scan fetched body from database: %v", err.Error()))
 	}
 
+	// TODO: Encrypt and decrypt password
 	if Req.Password != StoredUser.Password {
 		return utils.SendErrorResponse(e, http.StatusBadRequest, "Password is incorrect")
 	}
